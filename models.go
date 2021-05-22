e4364d9eace28db1d2f3c31f1d8a7223f3d52bf7
Sean Smith <sean@wwsean08.com>
2021-05-21 23:55:06 -0700

    Add some functions and related tests

--- models.go
+++ models.go
@@ -1,5 +1,7 @@
 package go_tau
 
+import "time"
+
 type Event struct {
 	ID          string `json:"id"`
 	EventID     string `json:"event_id"`
@@ -7,6 +9,11 @@ type Event struct {
 	EventSource string `json:"event_source"`
 	Created     string `json:"created"`
 	Origin      string `json:"origin"`
+}
+
+func (e *Event) CreatedAsTime() (time.Time, error) {
+	// 2021-05-22T05:20:06.120452+00:00
+	return time.Parse("2006-01-02T15:04:05.999999999-07:00", e.Created)
 }
 
 type FollowMsg struct {
@@ -126,6 +133,20 @@ type HypeTrainBeginMsg struct {
 	} `json:"event_data"`
 }
 
+// StartedAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainBeginMsg) StartedAtAsTime() (time.Time, error) {
+	//2020-07-15T17:16:03.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.StartedAt)
+}
+
+// ExpiresAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainBeginMsg) ExpiresAtAsTime() (time.Time, error) {
+	//2020-07-15T17:16:11.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.ExpiresAt)
+}
+
 type HypeTrainProgressMsg struct {
 	*Event
 	EventData struct {
@@ -155,6 +176,20 @@ type HypeTrainProgressMsg struct {
 	} `json:"event_data"`
 }
 
+// StartedAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainProgressMsg) StartedAtAsTime() (time.Time, error) {
+	// 2020-07-15T17:16:03.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.StartedAt)
+}
+
+// ExpiresAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainProgressMsg) ExpiresAtAsTime() (time.Time, error) {
+	// 2020-07-15T17:16:03.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.ExpiresAt)
+}
+
 type HypeTrainEndedMsg struct {
 	*Event
 	EventData struct {
@@ -175,4 +210,25 @@ type HypeTrainEndedMsg struct {
 			Total     int    `json:"total"`
 		} `json:"top_contributions"`
 	} `json:"event_data"`
+}
+
+// StartedAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainEndedMsg) StartedAtAsTime() (time.Time, error) {
+	// 2020-07-15T17:16:03.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.StartedAt)
+}
+
+// EndedAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainEndedMsg) EndedAtAsTime() (time.Time, error) {
+	// 2020-07-15T17:16:03.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.EndedAt)
+}
+
+// CooldownEndsAtAsTime returns a Time object for the started at (which isn't needed often hence why
+// it's not doing the conversion by default)
+func (h *HypeTrainEndedMsg) CooldownEndsAtAsTime() (time.Time, error) {
+	// 2020-07-15T18:16:11.17106713Z
+	return time.Parse("2006-01-02T15:04:05.999999999Z", h.EventData.CooldownEndsAt)
 }
